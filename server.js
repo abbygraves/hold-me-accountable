4969747647e6b6d1c205f281f686dd6996c66ffe
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-08-07 21:15:15 -0500

    add .env.example and express-handlebars

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use(require('./controllers/'));
+// app.use(require('./controllers/'));
 
 sequelize.sync({force: false}).then(() => {
     app.listen(PORT, () => console.log('Listening'));
