bdf9d8e233650be2c646d302c7d6535e1a431d82
Devon <devondgray28@gmail.com>
2022-08-08 17:49:25 -0500

    packages

--- routes.js
+++ routes.js
@@ -1,6 +1,23 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+var mysql      = require('mysql');
+var connection = mysql.createConnection({
+  host     : 'example.org',
+  user     : 'bob',
+  password : 'secret'
+});
+
+connection.connect(function(err) {
+  if (err) {
+    console.error('error connecting: ' + err.stack);
+    return;
+  }
+
+  console.log('connected as id ' + connection.threadId);
+});
+
+
 // GET /api/users
 router.get('/', (req, res) => {});
 
