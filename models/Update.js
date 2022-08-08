07c8723dd1b8fb734323b9eb92f0054b96b8c1b9
DarioOrbita <104122781+DarioOrbita@users.noreply.github.com>
2022-08-08 17:21:33 -0500

    add update model and associations

---
+++ models/Update.js
@@ -0,0 +1,49 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class Update extends Model {}
+
+Update.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+
+    title: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    update_text: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [1],
+      },
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    post_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "post",
+        key: "id",
+      },
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "Update",
+  }
+);
+
+module.exports = Update;
--- models/index.js
+++ models/index.js
@@ -1,17 +1,29 @@
 const User = require("./User");
 const Post = require("./Post");
 const Comment = require("./Comment");
+const Update = require("./Update");
 
 //User to Post associations
-User.hasMany(Post, { foreignKey: "user_id" });
+User.hasOne(Post, { foreignKey: "user_id" });
 Post.belongsTo(User, { foreignKey: "user_id", onDelete: "set null" });
 
 //User to Comment associations
 User.hasMany(Comment, { foreignKey: "user_id", onDelete: "set null" });
 Comment.belongsTo(User, { foreignKey: "user_id", onDelete: "set null" });
 
+
+//User to Update associations
+User.hasMany(Update, { foreignKey: "user_id"});
+Update.belongsTo(User, { foreignKey: "user_id", onDelete: "set null"});
+
+//Post to Update associations
+Post.hasMany(Update, { foreignKey: "post_id" });
+Update.belongsTo(Post, { foreignKey: "post_id", onDelete: "set_null" });
+
+
 //Post to Comment associations
 Post.hasMany(Comment, { foreignKey: "post_id" });
 Comment.belongsTo(Post, { foreignKey: "post_id", onDelete: "set null" });
 
-module.exports = { User, Post, Comment };
+
+module.exports = { User, Post, Comment, Update };
