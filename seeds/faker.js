5b2a9209b6c571d6f8bf1a67f8fa8409bf3c294f
SpookyAura7 <handscrews@outlook.com>
2022-08-08 22:05:39 -0500

    comment and post data

--- seeds/faker.js
+++ seeds/faker.js
@@ -1,4 +1,7 @@
  const faker = require('faker');
+const { READUNCOMMITTED } = require('sequelize/types/table-hints');
+
+// user "data"
 
  const username = faker.internet.userName();
 
@@ -7,3 +10,18 @@
  const email = faker.internet.email();
 
  const profile_img = faker.image.animals(200, 200, true);
+
+ // comment "data"
+
+ const comment_text = faker.lorem.paragraph();
+
+ const user_id = faker.datatype.number(1000);
+
+ const post_id = faker.datatype.number(1000);
+
+ // post "data"
+
+ const title = faker.random.words(7);
+
+//not sure if I need this const post_url = 
+
