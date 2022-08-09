4b172a304bbd49f33bbcecf071af7bd56f915ae2
abbygraves <abbygraves14@gmail.com>
2022-08-09 18:49:09 -0500

    fixed profile image

--- server.js
+++ server.js
@@ -32,6 +32,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+
 app.use(require('./controllers/'));
 
 sequelize.sync({ force: false }).then(() => {
