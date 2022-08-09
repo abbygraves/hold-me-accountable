de088248864749996c8a0f9fc21eefa551fab63b
SpookyAura7 <handscrews@outlook.com>
2022-08-08 21:40:48 -0500

    faker data for users

---
+++ seeds/faker.js
@@ -0,0 +1,9 @@
+ const faker = require('faker');
+
+ const username = faker.internet.userName();
+
+ const password = faker.interent.password();
+
+ const email = faker.internet.email();
+
+ const profile_img = faker.image.animals(200, 200, true);
